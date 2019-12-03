fdb1498136dbf4d759aa885dd3be5f9396361f5c
Ligin Vellakkad <ligindd@gmail.com>
2019-12-03 20:25:05 +0530

    Study drill 01 & 02

--- ex05.rb
+++ ex05.rb
@@ -6,9 +6,13 @@ eyes = 'Black'
 teeth = 'White'
 hair = 'Black'
 
+cm_to_inches = height / 2.54
+kg_to_pounds = weight * 2.205
+
+
 puts "Let's talk about #{name}"
-puts "He's #{height} cm tall."
-puts "He's #{weight} kg heavy."
+puts "He's #{height} cm tall. In inches it's #{cm_to_inches} inches."
+puts "He's #{weight} kg heavy. In pounds it's  #{kg_to_pounds} pounds."
 puts "Actually that's a bit heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on what he eats."
