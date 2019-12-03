8bce08af58775db8dc30218845f4e6b738d0cf4d
Ligin Vellakkad <ligindd@gmail.com>
2019-12-03 20:18:57 +0530

    Add exercise 05.

---
+++ ex05.rb
@@ -0,0 +1,17 @@
+my_name = 'Ligin Vellakkad'
+my_age = 27 # not a lie in 2019
+my_height = 168 # cm
+my_weight = 82 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's a bit heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on what he eats."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
