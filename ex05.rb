2b486460fd2179d93e184aa8f0868edf5de511ae
Ligin Vellakkad <ligindd@gmail.com>
2019-12-03 20:19:40 +0530

    Study drill 01

--- ex05.rb
+++ ex05.rb
@@ -1,17 +1,17 @@
-my_name = 'Ligin Vellakkad'
-my_age = 27 # not a lie in 2019
-my_height = 168 # cm
-my_weight = 82 # kg
-my_eyes = 'Black'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'Ligin Vellakkad'
+age = 27 # not a lie in 2019
+height = 168 # cm
+weight = 82 # kg
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
 
-puts "Let's talk about #{my_name}"
-puts "He's #{my_height} cm tall."
-puts "He's #{my_weight} kg heavy."
+puts "Let's talk about #{name}"
+puts "He's #{height} cm tall."
+puts "He's #{weight} kg heavy."
 puts "Actually that's a bit heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on what he eats."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on what he eats."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
